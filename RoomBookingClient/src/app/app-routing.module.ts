45a849952eca2ea9abec72f7ee0232dfe467e8ce
Pushparaj <pushparajcsegce@gmail.com>
2021-03-15 08:34:42 +0530

    Added Routings

--- RoomBookingClient/src/app/app-routing.module.ts
+++ RoomBookingClient/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { CalenderComponent } from './calender/calender.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'admin/users',component:UsersComponent},
+  {path:'admin/rooms',component:RoomsComponent},
+  {path:'calender',component:CalenderComponent},
+  {path:'404',component:PageNotFoundComponent},
+  {path:'**',redirectTo:'/404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- RoomBookingClient/src/app/app.module.ts
+++ RoomBookingClient/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalenderComponent } from './calender/calender.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalenderComponent,
+    RoomsComponent,
+    UsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
